848822225d34b3ea2edf265fea3d358d4a33f289
miguel <a320704@uach.mx>
2020-11-15 22:20:34 -0700

    Primer commit

---
+++ src/components/Contador.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+const Contador = () => {
+
+    const [contador, setContador] = useState(0)
+
+    return (
+        <div>
+            <p>{ contador }</p>
+
+            <button onClick={ () => setContador(contador + 1) }>Agregar</button>
+        </div>
+    )
+}
+
+export default Contador
---
+++ src/components/Parrafo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Parrafo = () => {
+    return (
+        <div>
+            <p>Lorem lorem lorem lorem lorem lorem</p>
+        </div>
+    )
+}
+
+export default Parrafo
